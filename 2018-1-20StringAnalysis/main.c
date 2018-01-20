8391b251c673079d01a1c6e141ee7cfee63b305d
zcfan <zchaofan08@gmail.com>
2018-01-20 16:28:10 +1100

    appendStr And concatStr

---
+++ 2018-1-20StringAnalysis/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* appendStr(char* s, char c)
+{
+        int len = strlen(s);
+        char* s2 = (char*)malloc(sizeof(char) * (len+1));
+        strcpy(s2, s);
+        s2[len] = c;
+        s2[len+1] = '\0';
+        printf("strAppend: %s", s);
+        return s2;
+}
+
+char* concatStr(char* str1, char* str2){
+    char* str3 = (char*)malloc(1 + strlen(str1) + strlen(str2));
+    strcpy(str3, str1);
+    strcat(str3, str2);
+    printf("%s", str3);
+    return str3;
+}
+
+
+int main(){
+    char *w = "uncomfortable";
+    char *w2 = "uncommon";
+    char *w3 = "unconditionally";
+    char *w4 = "unconscious";
+    printf("Your name is %s.", w);
+
+    char *common;
+    int size = strlen(w);
+    int size2;
+    char *rs="ll";
+    printf("\nlength: %d", size);
+    printf("\ntest concat: %s", concatStr("hello "," world"));
+    printf("\ntest append: %s", appendStr("hello ",'c'));
+
+    for(int i=0; i<=size-1; i++){
+        size2 = strlen(w2);
+        for(int j=0; j<=size2-1; j++){
+            if(w[i]==w2[j]){
+                // rs = strAppend(rs,w2[j]);
+                
+                printf("\ncommon: %c ", w2[j]);
+                 printf("\ncommon: %s ", rs);
+            }
+        }
+        // printf("\nletter: %c", w[i]);
+    }
+}
--- 20180103CLearning/read_variableLengthString.c
+++ 20180103CLearning/read_variableLengthString.c
@@ -1,10 +1,26 @@
-char* str;  
-char* _str; 
-
-int i = 1;  
-str = (char*)malloc(sizeof(char) * (i + 1));
-while('\n' != (str[i - 1] = getchar()))
+char* getstr()  
 {  
-     i++;  
-     ...  
-}
+        char* str;  
+        char* _str;  
+        int i = 1;  
+        str = (char*)malloc(sizeof(char) * (i + 1));  
+        while('\n' != (str[i - 1] = getchar()))  
+        {  
+                i ++;  
+                _str = (char*)malloc(strlen(str) + 1); 
+                str[i - 1] = '\0'; 
+                strcpy(_str, str);  
+                free(str);  
+                str = (char*)malloc(sizeof(char) * (i + 1));  
+                if(NULL == str)  
+                {  
+                        free(_str);  
+                        printf("No enough memory!");  
+                        return NULL;  
+                }  
+                strcpy(str, _str);  
+                free(_str);  
+        }  
+        str[i - 1] = '\0';  
+        return str;  
+}  
